7d723cfd06ef1e4f63a9b7c004cf58c91f668341
suzyhan <129558186+suzyhan@users.noreply.github.com>
2023-09-24 19:31:44 -0600

    Update asset caching

--- client/src-sw.js
+++ client/src-sw.js
@@ -29,7 +29,7 @@ registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 // TODO: Implement asset caching
 registerRoute(
   ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
-  new StaleWhileRevalidate({
+  new CacheFirst({
     cacheName: 'asset-cache',
     plugins: [
       new CacheableResponsePlugin({
